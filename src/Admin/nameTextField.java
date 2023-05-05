2e965503d9ff4ffc63b3da34c890336434b26b06
Davidongora <65700137+Davidongora@users.noreply.github.com>
2023-05-05 08:47:18 +0300

    Add files via upload

---
+++ src/Admin/nameTextField.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Admin;
+
+/**
+ *
+ * @author user
+ */
+class nameTextField {
+
+    static String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
